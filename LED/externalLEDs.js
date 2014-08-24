4065dcc9aa6109d08acc0c966f8d6ae85aa3c082
Srinath Sridhar <ssridhar@lap-14-42.mpi-inf.mpg.de>
2014-08-24 13:12:56 +0200

    Found lots of working GPIO pins. Logged in file. Still some pins need to be checked

--- LED/externalLEDs.js
+++ LED/externalLEDs.js
@@ -1,26 +1,32 @@
 var b = require('bonescript');
 
-var leds = [ "P8_13", "P8_14", "P8_15", "P8_16" ];
+var leds = [ "P8_7", "P8_8", "P8_9", "P8_10"
+	   , "P8_11", "P8_12", "P8_13", "P8_14" 
+	   , "P8_15", "P8_16", "P8_17", "P8_18"
+	   , "P9_11", "P9_12", "P9_13"
+	   ];
+//var nLEDs = 10;
+var nLEDs = leds.length;
 
-for (var i = 0; i < leds.length; ++i) {
+for (var i = 0; i < nLEDs; ++i) {
     b.pinMode(leds[i], b.OUTPUT, 7);
     b.digitalWrite(leds[i], b.LOW);
 }
 
 var currentpin = 0;
 var direction = 1;
-setInterval(nextpin, 300);
+setInterval(nextpin, 1);
 
 function nextpin() {
-    if(direction == 1 && currentpin >= leds.length - 1) {
-        currentpin = leds.length - 1;
+    if(direction == 1 && currentpin >= nLEDs - 1) {
+        currentpin = nLEDs - 1;
         direction = -1;
     } else if(direction == -1 && currentpin <= 0) {
         currentpin = 0;
         direction = 1;
     }
     
-    for (var i = 0; i < leds.length; ++i) {
+    for (var i = 0; i < nLEDs; ++i) {
         b.pinMode(leds[i], b.OUTPUT, 7);
         b.digitalWrite(leds[i], b.LOW);
     }
